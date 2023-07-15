34f532cc6198afc3849276f9157b238a59133255
Eduardo de Oliveira Sousa <84216150+eduardo-559@users.noreply.github.com>
2023-07-15 03:58:26 -0300

    Add files via upload

---
+++ ap2/express/crud/routes/alunos.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var alunoServiceMongo = require("../services/aluno.services.mongo")
+
+
+router.get(
+    "/listar"
+    ,
+    (request, response, next) => {
+        alunoServiceMongo.list(request,response)
+    }
+)
+
+router.post(
+    "/register"
+    ,
+    (request, response, next) => {
+        alunoServiceMongo.register(request,response)
+    }
+)
+
+router.put(
+    "/update/:id"
+    ,
+    (request, response, next) => {
+        alunoServiceMongo.update(request,response)
+    }
+)
+
+router.delete("/delete/:id"
+    ,
+    function (request, response, next) {
+        alunoServiceMongo.delete(request,response)
+    }
+)
+
+
+
+router.get('/retrieve/:id', function (request, response, next) {
+    alunoServiceMongo.retrieve(request,response)
+});
+
+module.exports = router;
