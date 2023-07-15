0f34e85986c9fd0803a15dc4a0ff9378dd11dda6
Eduardo de Oliveira Sousa <84216150+eduardo-559@users.noreply.github.com>
2023-07-15 02:29:30 -0300

    Delete ap2 directory

--- ap2/crud_expres_mongoose-back-end/src/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import LoginController from "../controllers/loginController.js";
-
-const router = express.Router();
-
-router
-    .post('/login',LoginController.logar)
-    .get('./login',LoginController.listarLogin)
